a992314b7d35bfdeccafa53da31cfb31daccf1c5
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-07-11 08:09:12 +0530

    added read operations

--- db/query/account.sql
+++ db/query/account.sql
@@ -5,3 +5,13 @@ INSERT INTO accounts (
     ) VALUES (
         $1,$2,$3
     )RETURNING *;
+
+--name: Genauthor :one 
+
+SELECT * FROM authors 
+WHERE id = $1 LIMIT 1; 
+
+--name: ListAuthors :many 
+
+SELECT * FROM authors 
+ORDER BY name;
